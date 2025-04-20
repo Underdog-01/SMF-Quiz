12ee9b1dd95ebfd0477ba6e72817042dd5b7bc3a
Chen Zhen <github.underdog@gmail.com>
2025-04-20 15:36:57 -0400

    Update Profile.php
    - extra permission checks for profile settings

--- Sources/Quiz/Profile.php
+++ Sources/Quiz/Profile.php
@@ -76,91 +76,95 @@ class Profile
 		global $context, $txt, $modSettings, $user_info, $scripturl, $smcFunc;
 
 		$context['profile_fields'] = !empty($context['profile_fields']) ? $context['profile_fields'] : [];
-		$member_info = [];
+		list($allowed, $member_info) = [false, []];
 
 		// Current user settings which are not adjusted in profile ~ (int)'' = 0
-		if ($user_info['id'] == $memID || empty($memID)) {
+		if ($user_info['id'] == $memID || empty($memID) && allowedTo('quiz_profile')) {
+			$allowed = true;
 			foreach (['quiz_pm_report', 'quiz_pm_alert', 'quiz_count'] as $key => $userSet) {
 				$member_info[$userSet] = !empty($user_info[$userSet]) ? $user_info[$userSet] : '';
 			}
 		}
-		else {
+		elseif (allowedTo('profile_identity_any')) {
+			$allowed = true;
 			$member_info = Helper::quiz_userPrefs($memID);
 		}
 
-		if (!empty($modSettings['SMFQuiz_enabled']) && allowedTo('quiz_admin')) {
-			$context['profile_fields'] += [
-				'quiz_pm_report' => [
-					'type' => 'check',
-					'label' => $txt['quiz_pm_report'],
-					'permission' => 'quiz_admin',
-					'enabled' => true,
-					'input_attr' => '',
-					'name' => 'quiz_pm_report',
-					'value' => !empty($member_info) ? (int)$member_info['quiz_pm_report'] : 0,
-				],
-			];
-		}
-		if (!empty($modSettings['SMFQuiz_enabled']) && (allowedTo('quiz_admin') || allowedTo('quiz_profile'))) {
-			$context['profile_fields'] += [
-				'quiz_pm_alert' => [
-					'type' => 'check',
-					'label' => $txt['quiz_pm_alert'],
-					'permission' => 'quiz_view',
-					'enabled' => true,
-					'input_attr' => '',
-					'name' => 'quiz_pm_alert',
-					'value' => !empty($member_info) ? (int)$member_info['quiz_pm_alert'] : 0,
-				],
-			];
-		}
+		if (!empty($allowed)) {
+			if (!empty($modSettings['SMFQuiz_enabled']) && allowedTo('quiz_admin')) {
+				$context['profile_fields'] += [
+					'quiz_pm_report' => [
+						'type' => 'check',
+						'label' => $txt['quiz_pm_report'],
+						'permission' => 'quiz_admin',
+						'enabled' => true,
+						'input_attr' => '',
+						'name' => 'quiz_pm_report',
+						'value' => !empty($member_info) ? (int)$member_info['quiz_pm_report'] : 0,
+					],
+				];
+			}
+			if (!empty($modSettings['SMFQuiz_enabled']) && (allowedTo('quiz_admin') || allowedTo('quiz_profile'))) {
+				$context['profile_fields'] += [
+					'quiz_pm_alert' => [
+						'type' => 'check',
+						'label' => $txt['quiz_pm_alert'],
+						'permission' => 'quiz_view',
+						'enabled' => true,
+						'input_attr' => '',
+						'name' => 'quiz_pm_alert',
+						'value' => !empty($member_info) ? (int)$member_info['quiz_pm_alert'] : 0,
+					],
+				];
+			}
 
-		if (isset($_REQUEST['save']))
-		{
-			checkSession('post');
-			list($errors, $updates) = array(false, []);
-
-			foreach ($context['profile_fields'] as $id => $field)
+			if (isset($_REQUEST['save']))
 			{
-				if ($id == 'notifications' || !isset($_POST[$id]))
-					continue;
+				checkSession('post');
+				list($errors, $updates) = array(false, []);
 
-				switch($field['type']) {
-					case 'check':
-						$_POST[$id] = (int)$_POST[$id];
-						break;
-					default:
-						if (isset($field['options'][$_POST[$id]])) {
-							$updates[] = [$memID, $id, $_POST[$id]];
-						}
+				foreach ($context['profile_fields'] as $id => $field)
+				{
+					if ($id == 'notifications' || !isset($_POST[$id]))
+						continue;
+
+					switch($field['type']) {
+						case 'check':
+							$_POST[$id] = (int)$_POST[$id];
+							break;
+						default:
+							if (isset($field['options'][$_POST[$id]])) {
+								$updates[] = [$memID, $id, $_POST[$id]];
+							}
+					}
 				}
+
+				$smcFunc['db_query']('', '
+					DELETE FROM {db_prefix}quiz_members
+					WHERE id_member = {int:memID}',
+					array(
+						'memID' => $memID,
+					)
+				);
+
+				foreach (array('quiz_pm_report', 'quiz_pm_alert', 'quiz_count') as $key => $userSet) {
+					$_POST[$userSet] = !empty($_POST[$userSet]) ? $_POST[$userSet] : '';
+				}
+
+				$smcFunc['db_insert']('insert',
+					'{db_prefix}quiz_members',
+					[
+						'id_member' => 'int', 'quiz_pm_report' => 'int', 'quiz_pm_alert' => 'int', 'quiz_count' => 'int',
+					],
+					[
+						$memID, (int)$_POST['quiz_pm_report'], (int)$_POST['quiz_pm_alert'],  (int)$user_info['quiz_count'],
+					],
+					['id_member']
+				);
+
+				unset($_REQUEST['save']);
+				redirectexit($scripturl . '?action=profile;area=quizProfileSettings;u=' . $memID);
 			}
-
-			$smcFunc['db_query']('', '
-				DELETE FROM {db_prefix}quiz_members
-				WHERE id_member = {int:memID}',
-				array(
-					'memID' => $memID,
-				)
-			);
-
-			foreach (array('quiz_pm_report', 'quiz_pm_alert', 'quiz_count') as $key => $userSet) {
-				$_POST[$userSet] = !empty($_POST[$userSet]) ? $_POST[$userSet] : '';
-			}
-
-			$smcFunc['db_insert']('insert',
-				'{db_prefix}quiz_members',
-				[
-					'id_member' => 'int', 'quiz_pm_report' => 'int', 'quiz_pm_alert' => 'int', 'quiz_count' => 'int',
-				],
-				[
-					$memID, (int)$_POST['quiz_pm_report'], (int)$_POST['quiz_pm_alert'],  (int)$user_info['quiz_count'],
-				],
-				['id_member']
-			);
-
-			unset($_REQUEST['save']);
-			redirectexit($scripturl . '?action=profile;area=quizProfileSettings;u=' . $memID);
 		}
 
 		$context['profile_custom_submit_url'] = $scripturl . '?action=profile;area=quizProfileSettings;u=' . $memID . ';save';
